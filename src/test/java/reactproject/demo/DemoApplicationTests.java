7d4577e4e7601452d4eb33df1467bc662881413f
whd07 <whd0714@naver.com>
2021-05-24 01:24:30 +0900

    Initial commit

---
+++ src/test/java/reactproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package reactproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
