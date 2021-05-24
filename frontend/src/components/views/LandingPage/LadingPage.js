12e917d22845aedea506a79654ba38cd22b73337
whd07 <whd0714@naver.com>
2021-05-24 15:48:53 +0900

    컴포넌트 추가

---
+++ frontend/src/components/views/LandingPage/LadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LadingPage(props) {
+    return (
+        <div>
+            LadingPage
+        </div>
+    );
+}
+
+export default LadingPage;
---
+++ frontend/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegisterPage(props) {
+    return (
+        <div>
+            RegisterPage
+        </div>
+    );
+}
+
+export default RegisterPage;
