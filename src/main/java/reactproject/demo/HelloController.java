f9a543c1eaf0d4ec160cd295c59e2313f5b003ed
whd07 <whd0714@naver.com>
2021-05-25 15:05:43 +0900

    redux setting

---
+++ src/main/java/reactproject/demo/HelloController.java
@@ -0,0 +1,13 @@
+package reactproject.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello~ world!";
+    }
+}
---
+++ src/main/java/reactproject/demo/setting/SecurityConfig.java
@@ -0,0 +1,50 @@
+package reactproject.demo.setting;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+/*    private final MemberService memberService;*/
+    private final DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up","/api/*").permitAll()
+                .anyRequest().authenticated();
+
+        http.logout().logoutSuccessUrl("/");
+
+        http.formLogin().loginPage("/login").permitAll();
+
+        /*http.rememberMe()
+                .userDetailsService(memberService)
+                .tokenRepository(tokenRepository());*/
+    }
+
+    private PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
